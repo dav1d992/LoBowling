4f57712c94b40edb3728988d27d6552679464ee7
dav1d992 <daviden@live.dk>
2022-06-20 22:50:57 +0200

    Added domain classes

--- Domain/BowlingGame.cs
+++ Domain/BowlingGame.cs
@@ -2,9 +2,7 @@
 public class BowlingGame
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Category { get; set; }
-    public int Number { get; set; }
-    public int Rating { get; set; }
+    public string PlayerName { get; set; }
+    public int Score { get; set; }
+    public ICollection<Frame> Frames { get; set; }
 }
---
+++ Domain/Frame.cs
@@ -0,0 +1,9 @@
+namespace Domain;
+public class Frame
+{
+    public int Id { get; set; }
+    public int FirstRoll { get; set; }
+    public int SecondRoll { get; set; }
+    public int? ThirdRoll { get; set; }
+
+}
