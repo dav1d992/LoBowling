43dc3163c1277fcda269d4cb0193d12139d8984a
dav1d992 <daviden@live.dk>
2022-06-20 21:12:14 +0200

    Cleaned up ignored files

---
+++ Domain/BowlingGame.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain;
+public class BowlingGame
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public int Number { get; set; }
+    public int Rating { get; set; }
+}
