8dc9d4132d40d0677993c022ee5284f4bc99ac4b
dav1d992 <daviden@live.dk>
2022-06-21 20:31:33 +0200

    Added data seeding

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,6 +12,26 @@ namespace Persistence
         }
 
         public DbSet<BowlingGame> BowlingGames { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Frame> Frames { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Frame>(x => x.HasKey(y =>
+                new { y.UserId, y.BowlingGameId, y.FrameNumber }));
+
+            builder.Entity<Frame>()
+                .HasOne(x => x.User)
+                .WithMany(x => x.Frames)
+                .HasForeignKey(x => x.UserId);
+
+            builder.Entity<Frame>()
+                .HasOne(x => x.BowlingGame)
+                .WithMany(x => x.Frames)
+                .HasForeignKey(x => x.BowlingGameId);
+        }
 
     }
 }
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -9,69 +9,65 @@ namespace Persistence
     {
         public static void SeedData(DataContext context)
         {
-            if (!context.BowlingGames.Any())
+
+            if (!context.Users.Any())
             {
-                var frames = new List<Frame>
+                var users = new List<User>
                 {
-                    new Frame{Id=1, FirstRoll = 1, SecondRoll=4},
-                    new Frame{Id=2, FirstRoll = 1, SecondRoll=5},
-                    new Frame{Id=3, FirstRoll = 1, SecondRoll=6},
-                    new Frame{Id=4, FirstRoll = 1, SecondRoll=7},
-                    new Frame{Id=5, FirstRoll = 1, SecondRoll=8},
-                    new Frame{Id=6, FirstRoll = 2, SecondRoll=7},
-                    new Frame{Id=7, FirstRoll = 2, SecondRoll=6},
-                    new Frame{Id=8, FirstRoll = 2, SecondRoll=5},
-                    new Frame{Id=9, FirstRoll = 2, SecondRoll=4},
-                    new Frame{Id=10, FirstRoll = 2, SecondRoll=3},
-                    new Frame{Id=11, FirstRoll = 5, SecondRoll=4},
-                    new Frame{Id=12, FirstRoll = 4, SecondRoll=5},
-                    new Frame{Id=13, FirstRoll = 3, SecondRoll=6},
-                    new Frame{Id=14, FirstRoll = 2, SecondRoll=7},
-                    new Frame{Id=15, FirstRoll = 1, SecondRoll=8},
-                    new Frame{Id=16, FirstRoll = 2, SecondRoll=7},
-                    new Frame{Id=17, FirstRoll = 3, SecondRoll=6},
-                    new Frame{Id=18, FirstRoll = 4, SecondRoll=5},
-                    new Frame{Id=19, FirstRoll = 5, SecondRoll=4},
-                    new Frame{Id=20, FirstRoll = 6, SecondRoll=3},
+                    new User
+                    {
+                        Id = 1,
+                        DisplayName = "Bob",
+                    },
+                    new User
+                    {
+                        Id = 2,
+                        DisplayName = "Jane",
+                    }
                 };
 
+                context.Users.AddRange(users);
+                context.SaveChanges();
+            }
+
+            if (!context.BowlingGames.Any())
+            {
                 var games = new List<BowlingGame>
                 {
                     new BowlingGame
                     {
-                        PlayerName = "Peter",
                         Score = 70,
                         Frames = new List<Frame>{
-                            new Frame{Id=1, FirstRoll = 1, SecondRoll=4},
-                            new Frame{Id=2, FirstRoll = 1, SecondRoll=5},
-                            new Frame{Id=3, FirstRoll = 1, SecondRoll=6},
-                            new Frame{Id=4, FirstRoll = 1, SecondRoll=7},
-                            new Frame{Id=5, FirstRoll = 1, SecondRoll=8},
-                            new Frame{Id=6, FirstRoll = 2, SecondRoll=7},
-                            new Frame{Id=7, FirstRoll = 2, SecondRoll=6},
-                            new Frame{Id=8, FirstRoll = 2, SecondRoll=5},
-                            new Frame{Id=9, FirstRoll = 2, SecondRoll=4},
-                            new Frame{Id=10, FirstRoll = 2, SecondRoll=3},
+                            new Frame{UserId=1, FrameNumber = 1, FirstRoll = 1, SecondRoll=4},
+                            new Frame{UserId=1, FrameNumber = 2, FirstRoll = 1, SecondRoll=5},
+                            new Frame{UserId=1, FrameNumber = 3, FirstRoll = 1, SecondRoll=6},
+                            new Frame{UserId=1, FrameNumber = 4, FirstRoll = 1, SecondRoll=7},
+                            new Frame{UserId=1, FrameNumber = 5, FirstRoll = 1, SecondRoll=8},
+                            new Frame{UserId=1, FrameNumber = 6, FirstRoll = 2, SecondRoll=7},
+                            new Frame{UserId=1, FrameNumber = 7, FirstRoll = 2, SecondRoll=6},
+                            new Frame{UserId=1, FrameNumber = 8, FirstRoll = 2, SecondRoll=5},
+                            new Frame{UserId=1, FrameNumber = 9, FirstRoll = 2, SecondRoll=4},
+                            new Frame{UserId=1, FrameNumber = 10, FirstRoll = 2, SecondRoll=3},
                         },
                     },
                     new BowlingGame
                     {
-                        PlayerName = "Johnny",
                         Score = 90,
                         Frames = new List<Frame>{
-                            new Frame{Id=11, FirstRoll = 5, SecondRoll=4},
-                            new Frame{Id=12, FirstRoll = 4, SecondRoll=5},
-                            new Frame{Id=13, FirstRoll = 3, SecondRoll=6},
-                            new Frame{Id=14, FirstRoll = 2, SecondRoll=7},
-                            new Frame{Id=15, FirstRoll = 1, SecondRoll=8},
-                            new Frame{Id=16, FirstRoll = 2, SecondRoll=7},
-                            new Frame{Id=17, FirstRoll = 3, SecondRoll=6},
-                            new Frame{Id=18, FirstRoll = 4, SecondRoll=5},
-                            new Frame{Id=19, FirstRoll = 5, SecondRoll=4},
-                            new Frame{Id=20, FirstRoll = 6, SecondRoll=3},
+                            new Frame{UserId=2, FrameNumber = 1, FirstRoll = 5, SecondRoll=4},
+                            new Frame{UserId=2, FrameNumber = 2, FirstRoll = 4, SecondRoll=5},
+                            new Frame{UserId=2, FrameNumber = 3, FirstRoll = 3, SecondRoll=6},
+                            new Frame{UserId=2, FrameNumber = 4, FirstRoll = 2, SecondRoll=7},
+                            new Frame{UserId=2, FrameNumber = 5, FirstRoll = 1, SecondRoll=8},
+                            new Frame{UserId=2, FrameNumber = 6, FirstRoll = 2, SecondRoll=7},
+                            new Frame{UserId=2, FrameNumber = 7, FirstRoll = 3, SecondRoll=6},
+                            new Frame{UserId=2, FrameNumber = 8, FirstRoll = 4, SecondRoll=5},
+                            new Frame{UserId=2, FrameNumber = 9, FirstRoll = 5, SecondRoll=4},
+                            new Frame{UserId=2, FrameNumber = 10, FirstRoll = 6, SecondRoll=3},
                         },
                     },
                 };
+
                 context.BowlingGames.AddRange(games);
                 context.SaveChanges();
             }
